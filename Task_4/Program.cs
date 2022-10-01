6d92afa693c52033c541a2b47f02f31354594a54
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-01 11:35:29 +0300

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить является ли число чётным
+int n = new Random().Next(1,20);
+int i = 1;
+
+if (n % 2 == 0)
+{
+    Console.WriteLine(n);
+    Console.WriteLine("Четно");
+}
+else
+{
+     Console.WriteLine(n);
+     Console.WriteLine("Нечетно");
+     n = n + i;
+}
