de6135407dcb3d773611568cd28a81ac5847bf23
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-02 02:38:28 +0300

    Task_11

---
+++ Task_8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+size = 99;
+n<size;
+n>max;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
