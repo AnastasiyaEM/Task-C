4c3db13440ca05d0399fe11f4a605bb5ad120dd4
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-02 20:24:07 +0300

    Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int numberA = new Random().Next(51,201);
+int numberB = new Random().Next(1,51);
+int result = numberA % numberB;
+
+if (result == 0)
+{
+   Console.Write($"Число {numberA} кратно числу {numberB}");
+}
+else
+{
+   double ost = numberA % numberB;
+
+   Console.WriteLine($"Число {numberA} не кратно числу {numberB}");
+   
+   Console.WriteLine("Остаток от деления: ");
+   Console.WriteLine(ost);
+}
