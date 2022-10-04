251fcc74c62f3735dcc4ac29618e24c631f02f20
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-04 23:05:26 +0300

    Task_13 - повтор первой задачи

---
+++ Task_13/Program.cs
@@ -0,0 +1,12 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+double n1 = 5;
+double n2 = 16;
+
+if(n1 * n1 == n2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
