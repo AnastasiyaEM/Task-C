017f7cbfd5f5bbc1460e03d5c3400df0c7c22710
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-09 16:13:34 +0300

    Task_19

---
+++ ДЗ3/Task_19/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+
+int A =1;
+int B = 4;
+int count = 0;
+int C = 1;
+int stepen = 3;
+int new_numberA = 0;
+
+while ( count < stepen)
+{
+    C = C * A;
+    A++;
+    C = C % 10;
+    new_numberA = C % 2 == 0;
+    Console.WriteLine ($"Число {A} заканчивается на четную цифру в кубе {new_numberA}");
+}
