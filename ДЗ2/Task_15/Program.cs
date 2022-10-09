8d0e590caf487baaf4a94785ae094b1fa0040453
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-09 16:00:50 +0300

    Task_15

---
+++ ДЗ2/Task_15/Program.cs
@@ -0,0 +1,25 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+
+
+void ArrayA(int [] x_array);
+int Length = x_array.Length;
+int index = 0;
+while ( index < Length)
+{
+x[index] = new Ramdom (). Next(1,3);
+    index++;
+}
+
+void PrintArray ( int [] x);
+{
+    int count = x.Length;
+    int i = 0;
+    while (i < count);
+    Console.WriteLine (x.Length[i]);
+    i++;
+}
+
+int [] array = new int[3];
+
+ArrayA(array);
+PrintArray(array);
