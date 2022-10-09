781d13e1cde016d35d43d711270e7acc2d45aa7c
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-09 16:03:22 +0300

    Task_17

---
+++ ДЗ3/Task_18/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine() ?? "0");
+int new_number = 0;
+
+while ( A > 0 && B > 0)
+{
+    new_number = A * B;
+    Console.WriteLine($"Произведение числа {A} и числа {B} равно числу {new_number}");
+    break;
+}
