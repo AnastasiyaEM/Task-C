dba0a9a576a53e9035abf1dafe5e6501777a2ca8
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-09 14:37:32 +0300

    Task_17

---
+++ ДЗ1/Task_17/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine(" Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+int i = 0;
+for (i = 0; i <= number; i++)
+{
+    sum = sum + number % 10;
+    number = number / 10;
+}
+Console.WriteLine(sum);
