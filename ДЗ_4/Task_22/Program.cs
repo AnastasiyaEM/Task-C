6212f034a63dd3f47394dd0de7abf773c8b3764e
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-14 01:47:21 +0300

    Task_22

---
+++ ДЗ_4/Task_22/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+Console.WriteLine("Введите размер массива: ");
+int sizeM = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[sizeM];
+Random number = new Random();
+Console.WriteLine("Сформированный массив: ");
+FillArrayRandomNum(array);
+PrintArray(array);
+int countA = 0;
+int countB = 0;
+
+void FillArrayRandomNum(int[] array) //заполнение массива
+{
+    for (int i = 0; i < array.Length; i++)  array[i] = number.Next(100, 1000);
+}
+
+void PrintArray(int[] array) //вывод строкой
+{
+    for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        countA++;
+    }
+    else if (array[i] % 2 != 0)
+    {
+        countB++;
+    }
+}
+Console.WriteLine($"Кол-во четных чисел в массиве: {countA}");
+Console.WriteLine($"Кол-во не четных чисел в массиве:  {countB}");
