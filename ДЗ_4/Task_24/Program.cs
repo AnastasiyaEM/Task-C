e65bb5ffbeb96c508d27551181f3faaabdd690da
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-14 02:18:11 +0300

    Task_24

---
+++ ДЗ_4/Task_24/Program.cs
@@ -0,0 +1,26 @@
+﻿//Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int[] array = new int[15];
+Random number = new Random();
+Console.WriteLine("Сформированный массив из 15 чисел: ");
+FillArray(array);
+PrintArray(array);
+int SumNumbers = 0;
+
+void FillArray(int[] array) //заполнение массива
+{
+    for (int i = 0; i < array.Length; i++) array[i] = number.Next(0, 100);
+}
+
+void PrintArray(int[] array) //вывод строкой
+{
+    for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+for (int i = 1; i < array.Length; i=i+2)
+{
+    SumNumbers+=array[i];
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел на нечетных позициях равна {SumNumbers}");
