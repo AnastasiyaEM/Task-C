b22a9d8007bd89aaca6cf1e4981de49f9771fd44
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-17 23:10:32 +0300

    Task_27(PART-1)

---
+++ ДЗ_5/Task_27 (PART-1)/Program.cs
@@ -0,0 +1,254 @@
+﻿// I. Реализовать следующие функции для работы с массивами:
+//1. Поиск минимума
+//2. Поиск максимума
+//3. Поиск суммы элементов массива
+//4. Поиск произведения элементов массива
+//5. Поиск индекса заданного элемента в массиве, если такого элемента в массиве нет то возвращать -1
+//6. Проверка наличия элемента в массиве. Возвращает true, если элемент в массиве есть, false – нет.
+//7. Печать массива на экран
+//8. Среднее арифметическое элеметов массива
+//9. Подсчёт количества отрицательных элементов массива
+//10. Подсчёт количества вхождений элемента в массив
+//11. Подсчёт количества чётных элементов в массив
+//12. Подсчёт количества положительных элементов в массиве
+//13. Подсчёт количества нечётных элементов в массиве
+//14. Проверка является ли массив отсортированным по возрастанию. Если массив отсортирован, то возвращать true, иначе - false.
+// II. Реализовать следующие функции:
+// 1. Функцию, которая вычисляет число a в степени n
+// 2. Функцию, которая вычисляет факториал числа n
+// 3. Функцию, которая вычисляет сумму цифр произвольного целого числа n
+// 4. Функцию, которая проверяет является ли заданное число n полиндромом
+// 5. Функцию, складывающую два целых числа
+// 6. Функцию, определяющую является ли число простым, то есть возвращающую true, если число простое, иначе - false
+// 7. Функцию, определяющую является ли число чётным, то есть возвращающую true, если число чётное, иначе - false
+
+// Заполнение массива
+void FillArray(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(min, max);
+}
+
+// Печать массива на экран
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i]);
+}
+
+// Поиск минимума
+int MinValue(int[] array)
+{
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] < min) min = array[i];
+
+    return min;
+}
+
+// Поиск максимума
+int MaxValue(int[] array)
+{
+    int max = array[0];
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > max) max = array[i];
+
+    return max;
+}
+
+// Поиск суммы элементов массива
+void SumArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+        sum += array[i];
+    Console.WriteLine(sum);
+}
+
+// Поиск произведения элементов массива
+void MultiplicationArray(int[] array)
+{
+    int mulriplication = 1;
+    for (int i = 0; i < array.Length; i++)
+        mulriplication *= array[i];
+    Console.WriteLine(mulriplication);
+}
+
+//Поиск индекса заданного элемента в массиве, если такого элемента в массиве 
+//нет то возвращать -1
+int IndexElement(int[] array, int N)
+{
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == N)
+        {
+            return i;
+        }
+    return -1;
+}
+
+// Проверка наличия элемента в массиве. Возвращает true, если элемент в массиве есть,
+//false – нет.
+bool ElementArray(int[] array, int value)
+{
+    bool x = false;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == value) x = true;
+    if (x == true) Console.WriteLine("Нашли число в массиве");
+    else Console.WriteLine("Не нашли число в массиве");
+    return x;
+}
+
+// Среднее арифметическое элеметов массива
+void MeanArray(int[] array)
+{
+    double sum = 0;
+    for (int i = 0; i < array.Length; i++)
+        sum = (sum + array[i]);
+    Console.WriteLine(sum / array.Length);
+}
+
+// Подсчёт количества отрицательных элементов массива
+int NegativeElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] < 0) count++;
+    return count;
+}
+
+// Подсчёт количества вхождений элемента в массив
+int Count(int[] array, int a)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == a) count++;
+    return count;
+}
+
+// Подсчёт количества чётных элементов в массив
+void ChetElement(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0) count++;
+    Console.WriteLine(count);
+}
+
+// Подсчёт количества положительных элементов в массиве
+int PositiveElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0) count++;
+    return count;
+}
+
+// Подсчёт количества нечётных элементов в массиве
+void NechetElement(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 != 0) count++;
+    Console.WriteLine(count);
+}
+
+// Проверка является ли массив отсортированным по возрастанию. 
+// Если массив отсортирован, то возвращать true, иначе - false.
+bool SortArray(int[] array)
+{
+    bool sort = true;
+    for (int i = 1; i < array.Length; i++)
+        if (array[i] < array[i - 1]) sort = false;
+    if (sort == false) Console.WriteLine();
+    else Console.WriteLine();
+    return sort;
+}
+
+
+
+Console.WriteLine("Размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+int[] array = new int[size];
+
+
+FillArray(array, 0, 10);
+Console.WriteLine("Массив: ");
+
+
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+
+
+int minvalue = MinValue(array);
+Console.WriteLine("Минимальное значение: " + minvalue);
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+
+
+int maxvalue = MaxValue(array);
+Console.WriteLine("Максимальное значение: " + maxvalue);
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+
+
+Console.WriteLine("Сумма элементов массива:");
+SumArray(array);
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+
+
+Console.WriteLine("Произведение элементов массива:");
+MultiplicationArray(array);
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+
+
+Console.WriteLine("Введите искомый элемент  массива:  (ищем индекс)");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+int result = IndexElement(array, N);
+Console.WriteLine($"Заданный элемент массива соответсвует индексу {result}");
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+
+
+Console.WriteLine("Проверим, есть ли число в массиве? Введите число: ");
+int value = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+bool elementArray = ElementArray(array, value);
+Console.WriteLine(elementArray);
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+
+
+Console.WriteLine("Среднее арифметическое элеметов массива:");
+MeanArray(array);
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+
+
+int NegElements = NegativeElements(array);
+Console.WriteLine("Кол-во отрицательных элементов в массиве: " + NegElements);
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+
+
+Console.WriteLine("С Вас число - с меня кол-во вхождений числа в массив: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+int count = Count(array, a);
+Console.WriteLine($"Заданное число встречается в массиве {count} раз.");
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+
+
+Console.WriteLine("Кол-во четных элементов в массиве:");
+ChetElement(array);
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+
+
+int PosElements = PositiveElements(array);
+Console.WriteLine("Кол-во положительных элементов в массиве: " + PosElements);
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+
+
+Console.WriteLine("Кол-во нечетных элементов в массиве:");
+NechetElement(array);
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+
+Console.WriteLine("Отсортирован ли Ваш массив? ");
+bool sortArray = SortArray(array);
+Console.WriteLine(sortArray);
+Console.WriteLine("|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
---
+++ ДЗ_5/Task_28(PART-2)/Program.cs
@@ -0,0 +1,122 @@
+﻿// II. Реализовать следующие функции:
+// 1. Функцию, которая вычисляет число a в степени 
+
+// int Stepen(int st, int number)
+// {
+//     int count = 0;
+//     int NewNumber = 1;
+//     for (int i = 0; i < st; i++)
+//         if (count < st) NewNumber *= number;
+//     count++;
+//     return NewNumber;
+// }
+
+// Console.WriteLine("Введите число, которое необходимо возвести в степень: ");
+// int number = int.Parse(Console.ReadLine() ?? "0");
+// Console.WriteLine("В какую степень возвести число?: ");
+// int st = int.Parse(Console.ReadLine() ?? "0");
+
+// int stepen = Stepen(st, number);
+// Console.WriteLine($"Число {number} в степени {st} равно {stepen}");
+
+
+
+// 2. Функцию, которая вычисляет факториал числа n
+
+// void Factorial(int N)
+// {
+//     int F = 1;
+//     for (int i = 1; i <= N; i++)
+//         F *= i;
+//     Console.WriteLine(F);
+// }
+
+// Console.WriteLine("Введите число искомого факториала: ");
+// int N = int.Parse(Console.ReadLine() ?? "0");
+
+// Console.WriteLine($"Факториал числа {N} равен ");
+// Factorial(N);
+
+
+
+// 3. Функцию, которая вычисляет сумму цифр произвольного целого числа n
+
+// void SumNumeral(int n)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < n; i++)
+//         if (n > 0)
+//         {
+//             sum += n % 10;
+//             n /= 10;
+//         }
+//     Console.WriteLine(sum);
+// }
+
+// Random rand = new Random();
+// int n = rand.Next(20, 40);
+// Console.WriteLine($"Сумма чисел в числе {n}");
+// SumNumeral(n);
+
+
+
+// 4. Функцию, которая проверяет является ли заданное число n полиндромом
+
+int Polindrome(string s)
+{
+    if (b[i]==b[int - 1])
+    
+    
+    
+    for (int i = 0; i < N / 2; ++i)
+        if (s[i] != s[s.Length - 1 - i]) return false;
+    return true;
+}
+
+
+
+
+int Polindrome(int num)
+{
+    int N = num;
+    int revers = 0;
+
+    while (N)
+    {
+        int r = n % 10; // последняя цифра
+        revers *= 10 + r; // 
+        N = N / 10;
+    }
+    return (num == revers);
+}
+
+Console.Write("Введите число и узнаем, является ли оно полиндромом ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+string n = N.ToString();
+
+Console.WriteLine("Число является полиндромом: 1- является, 0 - не является. ");
+Polindrome(num);
+
+
+// 5. Функцию, складывающую два целых числа
+
+// void SumNumbers(int numberA, int numberB)
+// {
+//     int newNumber = 0;
+//     if (numberA % 1 == 0 && numberB % 1 == 0) newNumber = numberA + numberB;
+//     Console.WriteLine(newNumber);
+// }
+
+// Console.WriteLine("Введите число №1: ");
+// int numberA = int.Parse(Console.ReadLine() ?? "0");
+// Console.WriteLine("Введите число №2: ");
+// int numberB = int.Parse(Console.ReadLine() ?? "0");
+
+// Console.WriteLine($"Сумма чисел {numberA} и {numberB} равна: ");
+// SumNumbers(numberA, numberB);
+
+// 6. Функцию, определяющую является ли число простым, то есть возвращающую true, если число простое, иначе - false
+
+
+
+// 7. Функцию, определяющую является ли число чётным, то есть возвращающую true, если число чётное, иначе - false
