8fcfd0e2fcc32894f83f59dfe857ae8fb82acc44
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-21 23:40:42 +0300

    Task_32

---
+++ ДЗ_6/Task_32/Program.cs
@@ -0,0 +1,35 @@
+﻿// Написать программу копирования массива
+
+void FillArray(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(min, max);
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i]);
+}
+
+void CopyArray(int[] array)
+{
+    int[] Copy = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Copy[i] = array[i];
+        Console.Write(Copy[i]);
+    }
+}
+
+Console.WriteLine("Размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+int[] array = new int[size];
+
+FillArray(array, 10, 30);
+Console.WriteLine($" Наш исходный массив: ");
+PrintArray (array);
+Console.WriteLine();
+
+Console.WriteLine($" Скопированный массив:  ");
+CopyArray(array);
