004ff2c44a10eda7a7a3f4f8442bf98e1e02cbee
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-10-24 21:36:21 +0300

    Task_30

--- ДЗ_6/Task_30/Program.cs
+++ ДЗ_6/Task_30/Program.cs
@@ -1,18 +1,23 @@
 ﻿// Показать числа Фибоначчи
 
-// f(1) =1
-// f(2) = 2
-// f(n) = f(n-1) + f(n-2)
-
 int Fibonachi(int n)
 {
-    if (n == 0 || n == 1) return 1;
+    if (n == 1|| n == 2) return 1;
     else return Fibonachi(n - 1) + Fibonachi(n - 2);
 }
 
-Console.WriteLine(" Введите искомое число Фибоначчи: ");
-int Num = int.Parse(Console.ReadLine() ?? "0");
-for (int i = 1; i <= Num; i++)
-{
-    Console.WriteLine($"f({i}) = {Fibonachi(i)}");
-}
+int n1 = Fibonachi(5);
+int n2 = Fibonachi(6);
+int n3 = Fibonachi(8);
+
+Console.WriteLine($" число Фибоначчи (5): {Fibonachi(5)}");
+Console.WriteLine($" число Фибоначчи (6): {Fibonachi(6)}");
+Console.WriteLine($" число Фибоначчи (8): {Fibonachi(8)}");
+
+
+// Console.WriteLine(" Введите искомое число Фибоначчи: ");
+// int Num = int.Parse(Console.ReadLine() ?? "0");
+// for (int i = 1; i <= Num; i++)
+// {
+//     Console.WriteLine($"f({i}) = {Fibonachi(i)}");
+// }
